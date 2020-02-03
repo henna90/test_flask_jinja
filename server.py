5eae91f7fa11a11b57fffcb2906fcca0159235e1
Henna Abbas <hennaabbas@hennas-mbp.lan>
2020-02-03 14:33:00 -0800

    added flash message to page & redirect if name is blank

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, session
+
+def is_person(person):
+    person = person.strip()
+    if person:
+        return True
+    else:
+        return False 
+
+      
+
 
 app = Flask(__name__)
+
+
+app.secret_key = "ABC"  
 
 # @app.route('/')
 # def index_page():
@@ -12,10 +25,14 @@ def greet_person():
         return render_template('index.html')
 
     else :
-        # person = request.args.get("person")
         person = request.form['person'] 
-
-        return render_template('index.html', person=person)  
+        if is_person(person):
+            session['person'] = person
+        # person = request.args.get("person")
+            return render_template('index.html', person=person)  
+        else:
+            flash("You enetered an empty string. Please enter your name again")
+            return redirect('/')  
 
 
 if __name__ == '__main__':
