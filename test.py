23d854d342a51454b964d460ce9b18268496b1ce
Henna <Henna.abbas0@gmail.com>
2020-06-03 15:15:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import unittest
 import xmlrunner
 import helpers
 
+
+
 class TestHelpers(unittest.TestCase):
     """unit tests: helper functions."""
 
@@ -25,6 +27,49 @@ class TestFlaskRoutes(unittest.TestCase):
 
             # Use the test client to make requests
         result = client.get('/')
+
+        # Compare result.data with assert method
+        self.assertIn(b'<h1>Welcome</h1>', result.data)
+#         self.assertIn(b'<h1>Bob</h1>', result.data)
+        self.assertEqual(result.status_code, 200)
+
+    def test_form(self):
+        """Test that / route processes form data correctly."""
+
+        client = server.app.test_client()
+        server.app.config['TESTING'] = True
+        result = client.post('/', data={'person': 'Henna'})
+        self.assertIn(b'Henna', result.data)  
+        self.assertEqual(result.status_code, 200)
+
+        def test_test(self):
+        """Test that / route processes form data correctly."""
+
+        client = server.app.test_client()
+        server.app.config['TESTING'] = True
+        result = client.post('/', data={'person': 'Henna'})
+        self.assertIn(b'Henna', result.data)  
+        self.assertEqual(result.status_code, 200)  
+
+        def fake_test(self):
+        """Test that / route processes form data correctly."""
+
+        client = server.app.test_client()
+        server.app.config['TESTING'] = True
+        result = client.post('/', data={'person': 'Henna'})
+        self.assertIn(b'Henna', result.data)  
+        self.assertEqual(result.status_code, 200)  
+          
+
+
+    
+if __name__ == "__main__":
+    unittest.main(
+        # testRunner=xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')),
+        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+        # these make sure that some options that are not applicable
+        # remain hidden from the help menu.
+        failfast=False, buffer=False, catchbreak=False)        
 
         # Compare result.data with assert method
         self.assertIn(b'<h1>Welcome</h1>', result.data)
