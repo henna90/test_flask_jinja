f930a996253562d520620e1aecd9df8dc952f622
Henna <Henna.abbas0@gmail.com>
2020-06-11 16:18:22 -0700

    Update test.py

--- user/test.py
+++ user/test.py
@@ -1,9 +1,38 @@
-class User(unittest.TestCase):
-    """unit tests: user function."""
 
-    def test_user():
-        """Test user function.""" 
-        self.assertEqual(user.user(), 'user')
+import unittest
+import xmlrunner
+import helpers
+
+
+
+# class User(unittest.TestCase):
+#     """unit tests: user function."""
+
+#     def test_user():
+#         """Test user function.""" 
+#         self.assertEqual(user.user(), 'user')
+        
+class TestHelpers(unittest.TestCase):
+    """unit tests: helper functions."""
+
+    def test_is_person(self):
+        """Test adder function."""
+
+        self.assertEqual(helpers.is_person('Henna'), True)
+#         self.assertEqual(helpers.is_person('. '), True)
+    def a(self):
+        """Test adder function."""
+
+        self.assertEqual(helpers.is_person('User'), True)
+    def b(self):
+        """Test adder function."""
+
+        self.assertEqual(helpers.is_person('Henna'), True)
+#         self.assertEqual(helpers.is_person('. '), True)
+    def z(self):
+        """Test adder function."""
+
+        self.assertEqual(helpers.is_person('Henna'), True)            
         
         
 if __name__ == "__main__":
