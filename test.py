4d33ef16bf5b44e712260d088eef6cf1f1c3e677
Henna <Henna.abbas0@gmail.com>
2020-06-11 18:20:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,16 +2,7 @@ import server
 import unittest
 import xmlrunner
 import helpers
-import user
-
-        
-class User(unittest.TestCase):
-    """unit tests: user function."""
-
-    def test_user():
-        """Test user function.""" 
-        self.assertEqual(user.user(), 'user')
-        
+      
         # wow cant remember python
 
 class TestHelpers(unittest.TestCase):
@@ -139,7 +130,7 @@ class TestFlaskRoutes(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main(
         # testRunner=xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')),
-        testRunner=xmlrunner.XMLTestRunner(output='user'),
+        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
         # these make sure that some options that are not applicable
         # remain hidden from the help menu.
         failfast=False, buffer=False, catchbreak=False)        
